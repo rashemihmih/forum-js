39a0e388d23dd6aeb40d57aba68f5ca82fdaa86c
rashemihmih <mihmih96@gmail.com>
2017-12-29 18:09:15 +0300

    mods

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ module.exports = function (app, db) {
         }
         db.collection('users').findOne({'login': login})
             .then((user) => {
-                if (user && passwordEncoder.matches(password, user.password) && user.isAdmin) {
+                if (user && passwordEncoder.matches(password, user.password) && (user.isAdmin || user.isMod)) {
                     req.session.login = login;
                     res.send(response.ok(login));
                 } else {
@@ -29,7 +29,7 @@ module.exports = function (app, db) {
         }
         db.collection('users').findOne({'login': login})
             .then((user) => {
-                if (user && user.isAdmin) {
+                if (user && (user.isAdmin || user.isMod)) {
                     res.send(response.ok(login));
                 } else {
                     res.send(response.authError())
@@ -39,7 +39,7 @@ module.exports = function (app, db) {
 
     app.delete('/admin/session', (req, res) => {
         let login = req.session.login;
-        if (login && user.isAdmin) {
+        if (login && (user.isAdmin || user.isMod)) {
             req.session.login = undefined;
             res.send(response.ok(login));
         } else {
@@ -55,7 +55,7 @@ module.exports = function (app, db) {
         }
         db.collection('users').findOne({'login': login})
             .then((user) => {
-                if (!user || !user.isAdmin) {
+                if (!user || !user.isMod) {
                     res.send(response.authError());
                     return;
                 }
@@ -134,7 +134,7 @@ module.exports = function (app, db) {
         }
         db.collection('users').findOne({'login': login})
             .then((user) => {
-                if (!user || !user.isAdmin) {
+                if (!user || !user.isMod) {
                     res.send(response.authError());
                     return;
                 }
@@ -154,7 +154,7 @@ module.exports = function (app, db) {
         }
         db.collection('users').findOne({'login': login})
             .then((user) => {
-                if (!user || !user.isAdmin) {
+                if (!user || !user.isMod) {
                     res.send(response.authError());
                     return;
                 }
@@ -163,5 +163,54 @@ module.exports = function (app, db) {
                 return post;
             }, () => res.send(response.dbError()))
             .then((post) => res.send(response.ok(post)), () => res.send(response.dbError()))
+    });
+
+    app.get('/admin/user/list', (req, res) => {
+        let login = req.session.login;
+        if (!login) {
+            res.send(response.authError());
+            return;
+        }
+        db.collection('users').findOne({'login': login})
+            .then((user) => {
+                if (!user || !user.isMod) {
+                    res.send(response.authError());
+                    return;
+                }
+                return db.collection('users').find({}).toArray();
+            }, () => res.send(response.dbError()))
+            .then((users) => {
+                if (users) {
+                    users.forEach((user) => delete user.password);
+                    res.send(response.ok(users))
+                }
+            }, () => res.send(response.dbError()))
+    });
+
+    app.post('/admin/user/mod', (req, res) => {
+        let login = req.session.login;
+        if (!login) {
+            res.send(response.authError());
+            return;
+        }
+        db.collection('users').findOne({'login': login})
+            .then((user) => {
+                if (!user || !user.isAdmin) {
+                    res.send(response.authError());
+                    return;
+                }
+                let modLogin = req.body.login;
+                let mod = req.body.mod;
+                if (!modLogin || (mod !== true && mod !== false)) {
+                    return response.incorrectRequest();
+                }
+                db.collection('users').updateOne({'login': modLogin}, {$set: {'isMod': mod}});
+                return modLogin;
+            }, () => res.send(response.dbError()))
+            .then((modLogin) => {
+                if (modLogin) {
+                    res.send(response.ok(modLogin))
+                }
+            }, () => res.send(response.dbError()))
     });
 };
